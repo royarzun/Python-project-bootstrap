41b4682bc8d345efa8e4ae2286d6908182aefe8b
Ricardo Oyarzun <royarzun@gmail.com>
2016-08-15 18:27:59 -0300

    Add main directories and init files

--- python_bootstrap/generator/PythonProject.py
+++ python_bootstrap/generator/PythonProject.py
@@ -1,10 +1,11 @@
 # coding=utf-8
 
 from python_bootstrap.utils.SystemUtils import create_folder,\
-    create_file_from_template, make_executable
+    create_file_from_template, make_executable, get_username
 
 TEMPLATE_SETUP = "python_setup.txt"
 TEMPLATE_CLASS = "python_class.txt"
+TEMPLATE_INIT = "python_init.txt"
 TEMPLATE_TESTS = "python_test.txt"
 
 
@@ -26,7 +27,12 @@ class PythonProject(object):
         """
         create_folder(self._root_folder)
         create_folder(self._main_package)
+        create_file_from_template(self._main_package + "/__init__.py",
+                                  TEMPLATE_INIT, author=get_username())
         create_folder(self._test_package)
+
+        create_file_from_template(self._test_package + "/__init__.py",
+                                  TEMPLATE_INIT, author=get_username())
 
     def _create_project_setup(self):
         setup_file_path = self._root_folder + "/setup.py"
@@ -40,11 +46,7 @@ class PythonProject(object):
     def _create_project_tests(self):
         pass
 
-    def _create_main_package(self):
-        pass
-
     def create(self):
         self._create_directories()
         self._create_project_setup()
-        self._create_main_package()
         self._create_project_tests()
