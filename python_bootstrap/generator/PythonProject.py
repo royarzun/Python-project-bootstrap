244db625ae3a0182d48ca5b93b93ee797601ee73
Ricardo Oyarzun <royarzun@gmail.com>
2016-08-15 13:29:57 -0300

    Add Python project skeleton

---
+++ python_bootstrap/generator/PythonProject.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+
+class PythonProject(object):
+
+    def __init__(self, project_name, **kwargs):
+        self.project_name = project_name
+        pass
+
+    def _create_project_setup(self):
+        pass
+
+    def _create_project_tests(self):
+        pass
+
+    def _create_main_package(self):
+        pass
+
+    def create(self):
+        pass
